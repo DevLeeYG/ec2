9a552356f275729639f71dc70bbdfdf7286911fa
DevLeeYG <hi33741@naver.com>
2022-04-04 22:47:09 +0900

    2

--- src/main/java/com/example/demo/controller/HealthCheckController.java
+++ src/main/java/com/example/demo/controller/HealthCheckController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,9 @@ public class HealthCheckController {
 	public String healthCheck() {
 		return "The service is up and running...";
 	}
+	@GetMapping("favicon.ico") @ResponseBody
+	public void returnNoFavicon() { }
+
+
 
 }
