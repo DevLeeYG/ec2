ec394f1eec5e8238e45706044fdf740ba0f899e2
DevLeeYG <hi33741@naver.com>
2022-04-14 17:00:43 +0900

    3

--- src/main/java/com/example/demo/controller/HealthCheckController.java
+++ src/main/java/com/example/demo/controller/HealthCheckController.java
@@ -1,15 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class HealthCheckController {
 
 	@GetMapping("/")
-	public String healthCheck() {
-		return "The service is up and running...";
+	public ResponseEntity healthCheck() {
+		return new ResponseEntity("success", HttpStatus.OK);
 	}
 	@GetMapping("favicon.ico") @ResponseBody
 	public void returnNoFavicon() { }
